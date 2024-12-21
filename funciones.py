b21732190a54751f3aa98828a4f5566277b6bb3f
y-Andrea <andrea19989620111@gamil.com>
2024-12-20 19:58:48 -0500

    Clase de funciones

---
+++ funciones.py
@@ -0,0 +1,18 @@
+#name = input ("enter your name")
+#print(name)
+#age = input ("enter you age")
+#print(age)
+
+#print("what is you name?")
+#name = input ()
+#print ("hello, how are you, " , name)
+
+#mi funcion, saludo.
+def grate_people() :
+    print("what is your name?")
+    name = input ()
+    print ("hello, how are you?" , name)
+    
+
+#llamo mi funcion
+grate_people()
